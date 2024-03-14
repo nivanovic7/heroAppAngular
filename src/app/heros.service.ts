981b68d0f151de69c97c876cf0d61fe964e628b0
Nikola Ivanovic <ivanovic.nikola.ns@gmail.com>
2024-03-14 23:08:25 +0100

    Finished error hadling

--- src/app/heros.service.ts
+++ src/app/heros.service.ts
@@ -4,6 +4,7 @@ import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,9 @@ export class HerosService {
 
    getHeros():Observable<Hero[]>{
     this.messageService.addMessage("Fetched Heroes!")
-    return this.http.get<Hero[]>(this.url)
+    return this.http.get<Hero[]>(this.url).pipe(
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
    }
 
    getHero(id: number): Observable<Hero>{
@@ -24,6 +27,28 @@ export class HerosService {
    }
 
    updateHero(id: number, name: string){
-    this.messageService.addMessage("Hero updated!")
+    // this.http.put(`${this.url}/${id}`)
    }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.addMessage(`HeroService: ${message}`);
+  }
 }
+
+
